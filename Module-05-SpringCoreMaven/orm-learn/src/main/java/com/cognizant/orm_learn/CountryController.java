258221f6ba039b56bf81e8c819d1edce5b856ed2
Riya Gupta <riyagupta@Riyas-MacBook-Air.local>
2026-07-27 22:17:00 +0530

    Complete Module 5 - Exercise 9: REST Controller

---
+++ Module-05-SpringCoreMaven/orm-learn/src/main/java/com/cognizant/orm_learn/CountryController.java
@@ -0,0 +1,22 @@
+package com.cognizant.orm_learn.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.orm_learn.entity.Country;
+import com.cognizant.orm_learn.service.CountryService;
+
+@RestController
+public class CountryController {
+
+    @Autowired
+    private CountryService countryService;
+
+    @GetMapping("/countries")
+    public List<Country> getCountries() {
+        return countryService.getAllCountries();
+    }
+}
